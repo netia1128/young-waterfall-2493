a368646b66536636650c01fdff04f5770f7d7c28
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-01 10:14:51 -0600

    Add model tests and code for actor #currently_working and #order_by_age

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,5 @@
 class Studio < ApplicationRecord
   has_many :movies
+
+  
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Actor do
+  before(:each) do
+    @actor1 = Actor.create(name: 'Luke Skywalker', age: 28, currently_working: true)
+    @actor2 = Actor.create(name: 'Princess Leah', age: 29, currently_working: false)
+    @actor3 = Actor.create(name: 'Emma Watson', age: 24, currently_working: true)
+    @actor4 = Actor.create(name: 'Rupert Grint', age: 25, currently_working: false)
+    @actor5 = Actor.create(name: 'Daniel Radcliffe', age: 23, currently_working: true)
+  end
+
   describe 'relationships' do
     it {should have_many :actor_movies}
     it {should have_many(:movies).through(:actor_movies)}
+  end
+
+  describe 'class methods' do
+    describe '#order_by_age' do
+      it 'should order all actors by age, oldest to youngest' do
+        expect(Actor.order_by_age).to eq([@actor2, @actor1, @actor4, @actor3, @actor5])
+      end
+    end
+    describe '#currently_working' do
+      it 'should return only actors that are currently working' do
+        expect(Actor.currently_working).to eq([@actor1, @actor3, @actor5])
+      end
+    end
   end
 end
